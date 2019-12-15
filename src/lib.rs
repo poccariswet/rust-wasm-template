0cc3bfba9603fa5aa0b7ea55d6f7138333d664dc
soe <s1240231@gmail.com>
2019-12-15 15:01:26 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,20 @@ pub fn initialize() {
     utils::set_panic_hook();
 }
 
+// it can use console.log on web console
 #[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(msg: &str);
     fn alert(s: &str);
+}
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, {{project-name}}!");
+    console_log!("hi {{project-name}");
 }
